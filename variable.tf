97729500e0d9b45b4b61f6e23cb31385cfa94753
WasimHannure <w.hannure@gmail.com>
2025-03-08 18:26:02 +0530

    updated with credential keys

--- variable.tf
+++ variable.tf
@@ -4,3 +4,9 @@ variable "instance_id" {
 variable "type" {
   description = "instance type"
 }
+variable "Access_key_ID" {
+  description = "access key of aws account"
+}
+variable "Secret_access_key" {
+  description = "secret access key of aws account"
+}
