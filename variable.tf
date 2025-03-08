f41701a3f4a95e2d62f5098f68dcd5f3e2b9b68d
WasimHannure <w.hannure@gmail.com>
2025-03-08 20:26:59 +0530

    updated with new provider.tf without creds

--- variable.tf
+++ variable.tf
@@ -1,10 +1,3 @@
 variable "instance_id" {
   description = "Value for ami ID"
 }
-
-variable "Access_key_ID" {
-  description = "access key of aws account"
-}
-variable "Secret_access_key" {
-  description = "secret access key of aws account"
-}
